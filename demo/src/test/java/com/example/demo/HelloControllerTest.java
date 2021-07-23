595ff08276ceaa9d009ec757e39b324024f55424
Maryam-n98 <mariam.mohannad98@gmail.com>
2021-07-23 21:24:06 +0300

    finished lab13

--- demo/src/test/java/com/example/demo/HelloControllerTest.java
+++ demo/src/test/java/com/example/demo/HelloControllerTest.java
@@ -8,7 +8,12 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.containsString;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -35,5 +40,10 @@ public class HelloControllerTest {
                 String.class)).contains("Hello World");
     }
 
+    @Test
+    public void testRequestStatus() throws Exception{
+        this.mockMvc.perform(get("/hello")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello World")));
+    }
 
     }
